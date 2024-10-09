a248e1cbf58a9dc884d5a763881bceffe6454653
Eliecer66 <jorge.eacostadiaz@gmail.com>
2024-10-08 21:44:02 -0300

    fixing config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   basePath: "/my-portfolio",
   output: "export",
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
