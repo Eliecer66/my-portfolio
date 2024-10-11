d0a1e0f63b97b865ce9785529c1d28b671f8b61f
Eliecer66 <jorge.eacostadiaz@gmail.com>
2024-10-11 00:00:58 -0300

    removing unused component

--- src/app/components/Projects/index.tsx
+++ src/app/components/Projects/index.tsx
@@ -12,7 +12,6 @@ import Image from 'next/image'
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
 
 const projects = [
   {
