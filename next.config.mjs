e63b01e8275e0c6bea2790b5e27c990e3eda80b4
Eliecer66 <jorge.eacostadiaz@gmail.com>
2024-11-17 22:31:51 -0300

    Adding configs for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  // basePath: "/my-portfolio",
-  // output: "export",
-  // reactStrictMode: true,
+  basePath: "/my-portfolio",
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
